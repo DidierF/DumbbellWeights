28a71dc158b71430db6199acd60f5c0e598e6193
Didier <didier.fuentes@gmail.com>
2025-10-14 18:54:59 -0400

    Refactors gradient mask to scrollview.

--- DumbbellWeights/Views/Components/WeightScrollView.swift
+++ DumbbellWeights/Views/Components/WeightScrollView.swift
@@ -47,5 +47,22 @@ struct WeightScrollView: View {
         }
       }
     }
+    .mask {
+      VStack(spacing: 0) {
+        LinearGradient(
+          colors: [.clear, .black],
+          startPoint: .top,
+          endPoint: .bottom
+        )
+        .frame(height: 75)
+        Rectangle().fill(Color.black)
+        LinearGradient(
+          colors: [.clear, .black],
+          startPoint: .bottom,
+          endPoint: .top
+        )
+        .frame(height: 75)
+      }
+    }
   }
 }
--- DumbbellWeights/Views/Screens/WorkoutView.swift
+++ DumbbellWeights/Views/Screens/WorkoutView.swift
@@ -121,24 +121,11 @@ struct WorkoutView: View {
 
         ChevronButton(.up) {}
 
-        WeightScrollView(items: allWeights, selectedItem: $baseWeight, onWeightPress: onWeightPress)
-          .mask {
-            VStack(spacing: 0) {
-              LinearGradient(
-                colors: [.clear, .black],
-                startPoint: .top,
-                endPoint: .bottom
-              )
-              .frame(height: 75)
-              Rectangle().fill(Color.black)
-              LinearGradient(
-                colors: [.clear, .black],
-                startPoint: .bottom,
-                endPoint: .top
-              )
-              .frame(height: 75)
-            }
-          }
+        WeightScrollView(
+          items: allWeights,
+          selectedItem: $baseWeight,
+          onWeightPress: onWeightPress
+        )
 
         ChevronButton(.down) {}
 
