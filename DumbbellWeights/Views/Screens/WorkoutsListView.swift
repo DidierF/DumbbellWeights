6d9f73368df6d93fdd4aa61ff7b8b403ce7642bf
Didier <didier.fuentes@gmail.com>
2025-10-18 18:41:58 -0400

    Shows saved workouts in history view.

--- DumbbellWeights/Views/Screens/WorkoutsListView.swift
+++ DumbbellWeights/Views/Screens/WorkoutsListView.swift
@@ -8,27 +8,69 @@
 import SwiftUI
 import SwiftData
 
+struct FormattedWorkout {
+  var title: String
+  var sets: [[Int]]
+}
+
 struct WorkoutsListView: View {
   @Query(sort: [SortDescriptor(\Workout.date)]) var workouts: [Workout]
+
+  init() {
+    UINavigationBar
+      .appearance().largeTitleTextAttributes = Helpers.getTitleAttributes()
+  }
+
+  var formattedWorkouts: [FormattedWorkout] {
+    []
+  }
+
+  func formatSets(_ sets: [ExerciseSet]) -> [String: [Int]] {
+
+    var result: [String: [Int]] = [:]
+
+    for aSet in sets {
+      let name = aSet.exercise.name
+      if result[name] == nil {
+        result[name] = []
+      }
+      result[name]?.append(aSet.weight)
+    }
+
+    return result
+
+  }
 
   var body: some View {
     BackgroundView {
       ScrollView {
-        VStack {
+        LazyVStack {
           ForEach(workouts) { workout in
-            Section(header:
-              Text(workout.date.formatted())
-              .padding()
+            Section(
+              header:
+                Text(workout.date.formatted())
+                .font(.system(size: 20, weight: .medium, design: .rounded))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding([.leading, .top])
             ) {
-              ForEach(workout.sets) { set in
-                HStack {
-                  Text(set.exercise.name)
-                  Text("\(set.weight)")
+              let sets = formatSets(workout.sets)
+              VStack(alignment: .leading) {
+                ForEach(Array(sets.keys.sorted()), id: \.self) {key in
+                  HStack {
+                    Text("\(key)")
+                    ForEach(sets[key] ?? [], id: \.self) { aSet in
+                      Text("\(aSet)")
+                    }
+                  }
+                  .frame(maxWidth: .infinity, alignment: .leading)
+                  .foregroundStyle(.primary1)
+                  .padding(.horizontal)
                 }
               }
+              .frame(maxWidth: .infinity)
             }
           }
-          .foregroundStyle(.white)
+          .foregroundStyle(.primary1)
         }
       }
       .scrollIndicators(.hidden)
@@ -37,5 +79,9 @@ struct WorkoutsListView: View {
 }
 
 #Preview {
-  WorkoutsListView()
+  NavigationStack {
+    WorkoutsListView()
+      .modelContainer(DataController.previewContainer)
+      .navigationTitle("Workouts")
+  }
 }
