e76287ec69b14f8391b766b61fded5a97d9c0b26
Didier <didier.fuentes@gmail.com>
2025-10-18 18:41:35 -0400

    Adds workouts to mock data.

--- DumbbellWeights/Services/DataController.swift
+++ DumbbellWeights/Services/DataController.swift
@@ -12,11 +12,58 @@ class DataController {
   static let previewContainer: ModelContainer = {
     do {
       let config = ModelConfiguration(isStoredInMemoryOnly: true)
-      let container = try ModelContainer(for: Exercise.self, configurations: config)
+      let container = try ModelContainer(
+        for: Exercise.self,
+        Workout.self,
+        configurations: config
+      )
 
       for i in 1...25 {
         let ex = Exercise("Exercise \(i)")
         container.mainContext.insert(ex)
+      }
+
+      for i in 1...10 {
+        let w = Workout()
+        w.sets = [
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise1\(i)"), weight: Int.random(in: 20...90)),
+
+          ExerciseSet(exercise: Exercise("Exercise2\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise2\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise2\(i)"), weight: Int.random(in: 20...90)),
+
+          ExerciseSet(exercise: Exercise("Exercise3\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise3\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise3\(i)"), weight: Int.random(in: 20...90)),
+
+          ExerciseSet(exercise: Exercise("Exercise4\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise4\(i)"), weight: Int.random(in: 20...90)),
+          ExerciseSet(exercise: Exercise("Exercise4\(i)"), weight: Int.random(in: 20...90)),
+        ]
+        container.mainContext.insert(w)
       }
 
       return container
