828bcd9f93d91180fd421efbc6a26cca7454d167
Didier <didier.fuentes@gmail.com>
2025-10-25 14:05:25 -0400

    Adds more exercises.

--- DumbbellWeights/Services/WorkoutService.swift
+++ DumbbellWeights/Services/WorkoutService.swift
@@ -8,16 +8,44 @@
 struct WorkoutService {
   func GetExercises() -> [[String]] {
     return [
-      ["Chest Press", "Chest"],
-      ["Bent Over Rows", "Back"],
-      ["Biceps Curl", "Biceps"],
-      ["Triceps Extension", "Triceps"],
-      ["Military Press", "Shoulders"],
-      ["Bent Over Flies", "Shoulders"],
-      ["Bulgarian Split Squat", "Legs"],
-      ["Goblet Squat", "Legs"],
-      ["Calf Raises", "Legs"],
+      ["Dumbbell Bench Press", "Chest"],
+      ["Push-Up", "Chest"],
+      ["Dumbbell Chest Fly", "Chest"],
+
+      ["Bent-Over Row", "Back"],
+      ["Reverse Fly", "Back"],
+      ["Superman Hold", "Back"],
+
+      ["Bicep Curl", "Biceps"],
+      ["Hammer Curl", "Biceps"],
+      ["Concentration Curl", "Biceps"],
+
+      ["Overhead Tricep Extension", "Triceps"],
+      ["Tricep Kickback", "Triceps"],
+      ["Close-Grip Push-Up", "Triceps"],
+
+      ["Overhead Press", "Shoulders"],
+      ["Lateral Raise", "Shoulders"],
+      ["Front Raise", "Shoulders"],
+
+      ["Plank", "Core"],
+      ["Russian Twist", "Core"],
+      ["Leg Raise", "Core"],
+
+      ["Farmer’s Carry", "Forearms"],
+      ["Wrist Curl", "Forearms"],
+      ["Reverse Curl", "Forearms"],
+
+      ["Squat", "Legs"],
+      ["Lunge", "Legs"],
       ["Romanian Deadlift", "Legs"],
+
+      ["Glute Bridge", "Glutes"],
+      ["Bulgarian Split Squat", "Glutes"],
+      ["Step-Up", "Glutes"],
+
+      ["Standing Calf Raise", "Calves"],
+      ["Seated Calf Raise", "Calves"]
     ]
   }
 }
